f17ecd8c5634f1419a9697897fc00228327a4d68
mak <spurosmakrugiannis7@gmail.com>
2026-03-04 10:57:55 +0200

    Add files via upload

---
+++ scanner.py
@@ -0,0 +1,18 @@
+import socket
+
+target = input("Enter target IP: ")
+
+print("Scanning target:", target)
+print("Scanning ports 1-1024")
+
+for port in range(1, 1025):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(0.5)
+
+    result = s.connect_ex((target, port))
+
+    if result == 0:
+        print("Port", port, "is open")
+
+    s.close()
+
